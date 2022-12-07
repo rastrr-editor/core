f5ca467ee8e882383b8d2fd7ac1fbf7fe181c6d0
Hunteraf <hunteraf@mail.ru>
2022-12-07 19:15:41 +0300

    Remove types (moved to global types)

--- src/color/interface.ts
+++ src/color/interface.ts
@@ -1,13 +1,1 @@
-type Enumerate<
-  N extends number,
-  Acc extends number[] = []
-> = Acc['length'] extends N
-  ? Acc[number]
-  : Enumerate<N, [...Acc, Acc['length']]>;
-
-type NumberRange<F extends number, T extends number> = Exclude<
-  Enumerate<T>,
-  Enumerate<F>
->;
-
 export type ColorRange = NumberRange<0, 256>;
