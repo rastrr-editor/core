f48f0f590eed2ccb5811a6da548fb7434b52a0fd
Hunteraf <hunteraf@mail.ru>
2022-12-07 19:00:27 +0300

    Reconfigure tsconfig. Reconfigure eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,9 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true,
