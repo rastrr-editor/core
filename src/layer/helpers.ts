7d6d40e8102c4fbd1f13585e15b0003e13297c18
Hunteraf <hunteraf@mail.ru>
2022-12-12 14:38:59 +0300

    Change calculate color range

--- src/layer/helpers.ts
+++ src/layer/helpers.ts
@@ -2,7 +2,7 @@ import { Color, ColorRange } from '~/color';
 
 export const toColorRange = (value: number): ColorRange => {
   const val = Math.round(value);
-  return val < 0 ? 0 : val > 255 ? 255 : (val as ColorRange);
+  return Math.max(0, Math.min(val, 255)) as ColorRange;
 };
 
 export const setColor = (
