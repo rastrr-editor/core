e91bdb4001c01bf89f426ecec2bdbd60c82237a6
Artem Shinkaruk <coder@neos1.com>
2022-12-14 00:24:19 +0700

    Configure package

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { Color } from './color';
 export type { ColorRange } from './color';
 export type { Layer, LayerOptions, LayerEmitter } from './layer';
 export { CanvasLayer, LayerFactory } from './layer';
+export * from './types';
