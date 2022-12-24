220ec6c70a8814b8c0684704f90c4bd89d8b263c
Artem Shinkaruk <coder@neos1.com>
2022-12-24 23:37:28 +0300

    Add includeLast option for every iterator helper

--- src/utils/async-iter/every.ts
+++ src/utils/async-iter/every.ts
@@ -1,9 +1,15 @@
+type EveryOptions = { includeLast?: boolean };
+
 export default async function* every<T>(
   iterable: AsyncIterable<T>,
-  predicate: (item: T) => boolean
+  predicate: (item: T) => boolean,
+  { includeLast = false }: EveryOptions = {}
 ): AsyncGenerator<T> {
   for await (const value of iterable) {
     if (!predicate(value)) {
+      if (includeLast) {
+        yield value;
+      }
       break;
     }
     yield value;
