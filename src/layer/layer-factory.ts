5dfe55b1d8bec93eb3437b31f5495aa76a45a739
Artem Shinkaruk <artasend@gmail.com>
2022-12-25 17:34:23 +0300

    Create DEFAULT_BACKGROUND const in layer factory

--- src/layer/layer-factory.ts
+++ src/layer/layer-factory.ts
@@ -2,6 +2,8 @@ import { Color } from '~/color';
 import { default as CanvasLayer } from './canvas-layer';
 import type { Layer, LayerType, LayerConstructor } from './interface';
 
+const DEFAULT_BACKGROUND = new Color(255, 255, 255, 255);
+
 export default class LayerFactory {
   protected static Class: LayerConstructor = CanvasLayer;
 
@@ -23,7 +25,11 @@ export default class LayerFactory {
     return new this.Class(w, h);
   }
 
-  static filled(w: number, h: number, color: Color): Layer {
+  static filled(
+    w: number,
+    h: number,
+    color: Color = DEFAULT_BACKGROUND
+  ): Layer {
     return new this.Class(w, h, { color });
   }
 
