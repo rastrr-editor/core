a5e825f2bf67476db61ed35502a1f594026f63ef
Artem Shinkaruk <artasend@gmail.com>
2022-12-28 22:53:37 +0700

    Fix map return type

--- src/utils/async-iter/map.ts
+++ src/utils/async-iter/map.ts
@@ -1,6 +1,6 @@
 export default async function* map<T, R>(
   iterable: AsyncIterable<T>,
-  mapFn: (item: T) => R
+  mapFn: (item: T) => R | Promise<R>
 ): AsyncGenerator<R> {
   for await (const value of iterable) {
     yield mapFn(value);
