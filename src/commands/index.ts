ad1bb2b3ea3a964843cfcc790a54962bf6bc4b96
Artem Shinkaruk <artasend@gmail.com>
2023-01-01 01:22:26 +0700

    Move rect command to shape commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,4 +2,4 @@ export type { Command, CommandOptions } from './interface';
 export { default as LayerCommand } from './layer-command';
 export { default as ShapeCommand } from './shape-command';
 export { default as BrushCommand } from './layer-commands/brush-command';
-export { default as RectCommand } from './layer-commands/rect-command';
+export { default as RectCommand } from './shape-commands/rect-command';
