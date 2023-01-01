3ebee7987b8e2adb27ae045cc9a6f17a1682d701
Artem Shinkaruk <artasend@gmail.com>
2023-01-01 15:42:59 +0700

    Create temporary layer in shape command

--- src/commands/shape-command.ts
+++ src/commands/shape-command.ts
@@ -3,8 +3,13 @@ import { createNewLayer } from '~/commands/helpers';
 import { LayerList } from '~/layer-list';
 
 export default abstract class ShapeCommand extends LayerCommand {
+  protected layers: LayerList;
+  protected insertIndex: number;
+
   constructor(layers: LayerList, iterable: AsyncIterable<Rastrr.Point>) {
-    const newLayer = createNewLayer(layers);
-    super(newLayer, iterable);
+    const { layer, index } = createNewLayer(layers, { tmp: true });
+    super(layer, iterable);
+    this.layers = layers;
+    this.insertIndex = index;
   }
 }
