7d93779468a8a7d4758e4c99e254d579227f8261
Artem Shinkaruk <artasend@gmail.com>
2023-01-01 15:49:32 +0700

    Refactor rect command
    - draw rect on temporary layer
    - create new layer same size as rect and draw final rect on it
    - do not create shape if user has clicked outside of the canvas
    - draw rect correctly from any direction (top-left, bottom-left, etc.)

--- src/commands/shape-commands/rect-command.ts
+++ src/commands/shape-commands/rect-command.ts
@@ -4,7 +4,10 @@ import { LayerList } from '~/layer-list';
 import {
   applyOptionsToCanvasCtx,
   applyDefaultOptions,
+  getLayerCanvasContext,
 } from '~/commands/helpers';
+import { LayerFactory } from '~/layer';
+import { Color } from '~/color';
 
 interface RectOptions extends CommandOptions {
   operation?: 'fill' | 'stroke';
@@ -27,32 +30,86 @@ export default class RectCommand extends ShapeCommand {
     let startPosition: Rastrr.Point | null = null;
     const { options, context, layer } = this;
     applyOptionsToCanvasCtx({
-      options,
+      // There we use same color as for brush cursor
+      options: { ...options, color: Color.from('#c1c1c1', 'hex') },
       context,
       layer,
-      operation: this.options.operation,
+      operation: 'stroke',
     });
     this.context.globalCompositeOperation = 'copy';
-
-    for await (const point of this.iterable) {
+    let width = 0;
+    let height = 0;
+    for await (const rawPoint of this.iterable) {
+      const point = { x: Math.round(rawPoint.x), y: Math.round(rawPoint.y) };
       if (!startPosition) {
+        if (
+          point.x < 0 ||
+          point.x > this.layer.canvas.width ||
+          point.y < 0 ||
+          point.y > this.layer.canvas.height
+        ) {
+          break;
+        }
         startPosition = point;
       }
-
       if (
         startPosition &&
         (startPosition?.x !== point.x || startPosition?.y !== point.y)
       ) {
-        const width = point.x - startPosition.x;
-        // TODO: detect shift pressed and make height equal to width
-        const height = point.y - startPosition.y;
-        this.context[
-          this.options.operation === 'fill' ? 'fillRect' : 'strokeRect'
-        ](startPosition.x, startPosition.y, width, height);
+        width = point.x - startPosition.x;
+        // TODO: detect if shift is pressed and make height equal to width
+        height = point.y - startPosition.y;
+        this.context.strokeRect(
+          startPosition.x,
+          startPosition.y,
+          width,
+          height
+        );
         this.layer.emitChange();
       }
     }
 
+    if (startPosition != null && width !== 0 && height !== 0) {
+      // Normalize width and height
+      if (height < 0) {
+        height = Math.abs(height);
+        startPosition.y -= height;
+      }
+      if (width < 0) {
+        width = Math.abs(width);
+        startPosition.x -= width;
+      }
+      const size = { x: width, y: height };
+      this.finalDraw(startPosition, size);
+    } else {
+      this.layers.remove(this.insertIndex);
+    }
     return true;
+  }
+
+  protected finalDraw(startPosition: Rastrr.Point, size: Rastrr.Point) {
+    const layer = this.layers.remove(this.insertIndex);
+    // Create new layer
+    const newLayer = LayerFactory.setType(layer.type).empty(size.x, size.y, {
+      opacity: layer.opacity,
+    });
+    newLayer.setOffset(startPosition);
+    this.layers.add(newLayer);
+    this.layers.setActive(this.layers.length - 1);
+
+    // Draw rect on new layer
+    const context = getLayerCanvasContext(newLayer);
+    const { options } = this;
+    applyOptionsToCanvasCtx({
+      options,
+      context,
+      layer,
+      operation: options.operation,
+    });
+    const method =
+      this.options.operation === 'fill' ? 'fillRect' : 'strokeRect';
+    context[method](0, 0, size.x, size.y);
+    newLayer.commitContentChanges();
+    newLayer.emitChange();
   }
 }
