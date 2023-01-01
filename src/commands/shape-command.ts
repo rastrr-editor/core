2cb6f3da1c455f377e32ba33bf6c306cf5309b28
Artem Shinkaruk <artasend@gmail.com>
2023-01-01 17:22:21 +0700

    Add shape command options

--- src/commands/shape-command.ts
+++ src/commands/shape-command.ts
@@ -1,15 +1,30 @@
 import { default as LayerCommand } from './layer-command';
-import { createNewLayer } from '~/commands/helpers';
+import { applyDefaultOptions, createNewLayer } from '~/commands/helpers';
 import { LayerList } from '~/layer-list';
+import { CommandOptions } from './interface';
+
+export interface ShapeCommandOptions extends CommandOptions {
+  getLayerName?: (baseName: string) => string;
+}
 
 export default abstract class ShapeCommand extends LayerCommand {
+  readonly options: ShapeCommandOptions;
   protected layers: LayerList;
   protected insertIndex: number;
 
-  constructor(layers: LayerList, iterable: AsyncIterable<Rastrr.Point>) {
+  constructor(
+    layers: LayerList,
+    iterable: AsyncIterable<Rastrr.Point>,
+    options?: Partial<ShapeCommandOptions>
+  ) {
     const { layer, index } = createNewLayer(layers, { tmp: true });
     super(layer, iterable);
     this.layers = layers;
     this.insertIndex = index;
+    this.options = applyDefaultOptions(options);
+  }
+
+  getLayerName() {
+    return this.options.getLayerName?.(this.name) ?? this.name;
   }
 }
