4dc8e3f0243b886a45782d9c1cd7522c9b4aa10d
Artem Shinkaruk <artasend@gmail.com>
2023-01-10 22:50:47 +0700

    Add backup property for layer command

--- src/commands/layer-command.ts
+++ src/commands/layer-command.ts
@@ -2,11 +2,21 @@ import { Layer } from '~/layer';
 import { getLayerCanvasContext } from './helpers';
 import { Command } from './interface';
 
+export type LayerBackupData = {
+  layerId: string;
+  imageData: ImageData;
+  area: {
+    start: Rastrr.Point;
+    end: Rastrr.Point;
+  };
+};
+
 export default abstract class LayerCommand implements Command {
   readonly name: string = 'unnamed';
   protected context: CanvasRenderingContext2D;
   protected layer: Layer;
   protected iterable: AsyncIterable<Rastrr.Point>;
+  protected backup?: LayerBackupData;
 
   constructor(layer: Layer, iterable: AsyncIterable<Rastrr.Point>) {
     this.layer = layer;
