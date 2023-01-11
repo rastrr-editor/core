8f55ee4901a800200c2b59272ca7b2b0028e56f1
Artem Shinkaruk <artasend@gmail.com>
2023-01-11 23:02:02 +0700

    Add createBackup method for layer command

--- src/commands/layer-command.ts
+++ src/commands/layer-command.ts
@@ -1,10 +1,14 @@
+import { Rectangle } from '~/geometry';
 import { Layer } from '~/layer';
+import { getAreaFromPoints } from '~/utils/aggregate';
 import { RewindableAsyncIterableIterator } from '~/utils/async-iter';
-import { getLayerCanvasContext } from './helpers';
+import { createDebug } from '~/utils/debug';
+import { extractImageDataForArea, getLayerCanvasContext } from './helpers';
 import { Command } from './interface';
 
+export const debug = createDebug('commands');
+
 export type LayerBackupData = {
-  layerId: string;
   imageData: ImageData;
   area: {
     start: Rastrr.Point;
@@ -42,4 +46,52 @@ export default abstract class LayerCommand implements Command {
   abstract execute(): Promise<boolean>;
 
   abstract undo(): Promise<boolean>;
+
+  protected createBackup(areaModifier = 0): void {
+    // Prevent backup override
+    if (this.backup != null) {
+      return;
+    }
+    const interactionArea = getAreaFromPoints(this.iterable.getBuffer());
+    const resizedArea: Rastrr.Area = {
+      start: {
+        x: interactionArea.start.x - areaModifier,
+        y: interactionArea.start.y - areaModifier,
+      },
+      end: {
+        x: interactionArea.end.x + areaModifier,
+        y: interactionArea.end.y + areaModifier,
+      },
+    };
+    const areaRect = new Rectangle(resizedArea);
+    const layerRect = new Rectangle(
+      this.layer.offset,
+      this.layer.width,
+      this.layer.height
+    );
+    const intersection = areaRect.intersection(layerRect);
+    if (intersection != null) {
+      const area = intersection.toArea();
+      const imageData = extractImageDataForArea(
+        getLayerCanvasContext(this.layer),
+        area.start,
+        area.end
+      );
+      if (imageData != null) {
+        debug(
+          'backup layer, name: %s, id: %s, area corner: (%d, %d), area size: %d x %d',
+          this.layer.name,
+          this.layer.id,
+          intersection.corner.x,
+          intersection.corner.y,
+          intersection.width,
+          intersection.height
+        );
+        this.backup = {
+          imageData,
+          area,
+        };
+      }
+    }
+  }
 }
