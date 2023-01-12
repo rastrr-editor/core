f5ce7141c3ae35b3ffd8f7cbea0ac6ab77650eca
Artem Shinkaruk <artasend@gmail.com>
2023-01-12 23:37:43 +0700

    Export history

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export * from './layer';
 export * from './layer-list';
 export * from './render';
 export * from './viewport';
+export * from './history';
 export * from './commands';
 export * from './utils';
